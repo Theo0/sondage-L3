f37939ab2a172e32efa3ea66e635079b7116d119
Théo <theo341@gmail.com>
2014-03-25 11:31:21 +0100

    Correction bugs et gestion des actions

--- controllers/ControllerOption.php
+++ controllers/ControllerOption.php
@@ -112,6 +112,25 @@ class ControllerOption extends Controller{
 			
 	}
 
+}	
 
-	
+
+	?>
+
+<?php
+	if( !empty( $_GET["action"] ) && empty($_GET["controller"])){ // Appel d'une méthode de la classe sans passer par l'index.php
+		$controller = new controllerOption();
+		if( method_exists( $controller , $_GET["action"] ) ){ // Vérification: la méthode demandée existe dans le contrôleur
+			if( !empty( $_GET["params"] ) ){
+				$controller->$_GET["action"]( $_GET["params"] ); // Exécution de l'action demandée avec des paramètres
+			}
+			else{
+				$controller->$_GET["action"](); // Exécution de l'action demandée sans paramètres
+			}
+		}
+		else{
+			$erreur = "Impossible d'effectuer l'action demandée";
+		}
+	}
 ?>
+
