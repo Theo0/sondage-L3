e4ade7238702c90294fbd9180ce55c5ba1e86527
Théo <theo341@gmail.com>
2014-03-26 19:13:35 +0100

    Create vueSondage.php

---
+++ views/vueSondage.php
@@ -0,0 +1,27 @@
+<div id="ficheSondage">
+<?php if(empty($FicheSondage)): ?>
+<p>Erreur : Aucun sondage séléctionné !</p>
+<?php else: ?>
+<h1><?php
+echo($FicheSondage->getTitre());
+ ?></h1>
+ <br />
+ <h3><?php
+echo($FicheSondage->getDesc());
+ ?></h3>
+ <br />
+<p>Ordonnez vos réponses parmis les choix ci-dessous : </p>
+<table id="tableListeSondages">
+	<thead>
+		<th>Option</th>
+	</thead>
+	<tbody>
+		<?php foreach($ListeOptions as $key=>$option){ ?>
+		<tr class="<?php if($key%2) echo 'ligneImpaire'; else echo 'lignePaire'; ?>">
+			<td><?php echo($option->getTexte()); ?></td>
+		</tr>
+		<?php } ?>
+	</tbody>
+</table>
+<?php endif; ?>
+</div>
