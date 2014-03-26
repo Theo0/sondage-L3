36397211bf716e499a5222e85814798371ef8d3a
Théo <theo341@gmail.com>
2014-03-26 16:06:29 +0100

    Update vueNouveauSondage.php
    Le formulaire n'utilise plus la classe form.
    
    Le groupe est récupéré depuis params dans l'URL, sinon le groupe est fixé à NULL.

--- views/vueNouveauSondage.php
+++ views/vueNouveauSondage.php
@@ -2,42 +2,32 @@
 
 $this->titre = "Nouveau Sondage";
 
-//Inclusion de la libarairie Form ( http://fr.openclassrooms.com/informatique/cours/votre-site-php-presque-complet-architecture-mvc-et-bonnes-pratiques/gestion-des-formulaires-avec-la-classe-form )
-include ROOT.'/models/Form.php';
+?> 
 
-// "formulaire_nouveau_sondage" est l'ID unique du formulaire
-$formulaire_nouveau_sondage = new Form('formulaire_nouv_sond', 'POST');
+<div>
 
-$formulaire_nouveau_sondage->action(ABSOLUTE_ROOT . '/controllers/ControllerSondage.php?action=nouveauSondage');
+<form method="POST" action="http://localhost/sondage-L3/controllers/ControllerSondage.php?action=nouveauSondage" id="formulaire_nouv_sond" >
+<p><label>Titre du sondage :   </label><input id="id_titre" name="titre" type="text" required="required" /></p>
+<p><label>Description :   </label><input id="id_description" name="description" type="text" required="required" /></p>
+<p><label>Visibilité :    </label><br />
+    <input id="id_visiblite_1" name="visiblite" type="radio" required="required" value="public" /> Public<br />
+    <input id="id_visiblite_2" name="visiblite" type="radio" required="required" value="inscrits" /> Inscrits<br />
+    <input id="id_visiblite_3" name="visiblite" type="radio" required="required" value="groupe" /> Groupe<br />
+    <input id="id_visiblite_4" name="visiblite" type="radio" required="required" value="prive" /> Privé<br /></p>
+<p><label>Date de fin :    </label><input name="date_fin" type="date" /></p>
+<p><label>Secret :   </label><br />
+    <input id="id_secret_1" name="secret" type="radio" value="secret" /> <label for="id_secret_1">secret</label><br />
+    <input id="id_secret_2" name="secret" type="radio" value="secret_scrutin" /> <label for="id_secret_2">secret_scrutin</label><br />
+    <input id="id_secret_3" name="secret" type="radio" value="public" /> <label for="id_secret_3">public</label><br /></p>    
+<input name="administrateur_id" type="hidden" value="<?php 
+echo $_SESSION['id']; 
+?>"/>
+<?php 
+if (isset($_GET['params'])){
+  echo '<input name="id_groupe" type="hidden" value="'.$_GET['params'].'"/>';
+}
+?>
 
-$formulaire_nouveau_sondage->add('Text', 'titre')
-                 ->label("Titre")->Required(true);
-		 
-$formulaire_nouveau_sondage->add('Text', 'description')
-                 ->label("Description")->Required(true);
-
-$formulaire_nouveau_sondage->add('Radio', 'visiblite')
-         ->choices(array(
-           'public' => 'public',
-           'inscrits' => 'inscrits',
-           'groupe' => 'groupe',
-           'prive' => 'prive'
-         ));
-		 //->label("Visibilite")->Required(true);
-
-$formulaire_nouveau_sondage->add('Date', 'date_fin')
-         ->format('dd/mm/yyyy');
-
-$formulaire_nouveau_sondage->add('Radio', 'secret')
-        ->choices(array(
-          'secret' => 'secret',
-          'secret_scrutin' => 'secret_scrutin',
-          'public' => 'public'
-          ));              
-
-$formulaire_nouveau_sondage->add('Submit', 'submit');    
-
-
-
-
-echo $formulaire_nouveau_sondage;
+<p><input name="submit" type="submit" /></p>
+</form>  
+</div>
