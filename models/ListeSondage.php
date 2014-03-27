723682348c528565673f3b3df11e384479636976
Théo <theo341@gmail.com>
2014-03-27 14:29:43 +0100

    Update ListeSondage.php

--- models/ListeSondage.php
+++ models/ListeSondage.php
@@ -15,20 +15,39 @@ class ListeSondage extends BD {
 		switch(func_num_args())
 		{
 			 case 0:
-			$this->constructeurVide();
+			$this->constructeurPublic();
 				break;
 		  	 case 1:
 			$this->cconstructeurListeAdministre(func_get_arg(0));
 				break;
 			case 2:
-			$this->constructeurListeAGroupe(func_get_arg(0), func_get_arg(1));
+			$this->constructeurListeGroupe(func_get_arg(0), func_get_arg(1));
+				break;
+			case 3:
+			$this->constructeurListeInscrit(func_get_arg(0), func_get_arg(1), func_get_arg(2));
+				break;
+			case 4:
+			$this->constructeurListeComplet(func_get_arg(0), func_get_arg(1), func_get_arg(2), func_get_arg(3));
+				break;
+			case 5:
+			$this->constructeurListePrive(func_get_arg(0), func_get_arg(1), func_get_arg(2), func_get_arg(3), func_get_arg(4));
 				break;
 
 		}
 	}
 
-	public function constructeurVide(){
-	    $this->array_sondage = array();
+	public function constructeurPublic(){
+	     $sql='SELECT id
+                    FROM sondage
+                 	WHERE visibilite="public"
+		    ORDER BY id DESC';
+                               
+            $lectBdd = $this->executerRequete($sql, array());
+            while (($enrBdd = $lectBdd->fetch()) != false)
+            { 
+
+                $this->array_sondage[] = new Sondage($enrBdd["id"]);
+            }
 	}
 
 	
@@ -38,6 +57,7 @@ class ListeSondage extends BD {
                     FROM sondage
                     WHERE administrateur_id=?
 		    ORDER BY id DESC';
+
                                
             $lectBdd = $this->executerRequete($sql, array($idUser));
             while (($enrBdd = $lectBdd->fetch()) != false)
@@ -47,7 +67,7 @@ class ListeSondage extends BD {
             }
 	}
 	
-	
+
 	public function constructeurListeGroupe($idUser, $idGroupe)
 	{ 
            $sql='SELECT id
@@ -62,6 +82,47 @@ class ListeSondage extends BD {
             }
 	}
 
+	public function constructeurListeInscrit($a, $b, $c){ //parametres factice pour choix du constructeur
+		 $sql='SELECT id
+                    FROM sondage
+                    WHERE visibilite="inscrits"
+		    ORDER BY id DESC';
+
+		 $lectBdd = $this->executerRequete($sql, array());
+            while (($enrBdd = $lectBdd->fetch()) != false)
+            { 
+                $this->array_groupes[] = new Sondage($enrBdd["id"]);
+            }   
+	}
+
+	public function constructeurListeComplet($idUser, $b, $c , $d){
+		$sql='SELECT id_sondage
+				FROM user_sondage_reponse
+				WHERE id_user=?
+				ORDER BY id DESC';
+		$lectBdd = $this->executerRequete($sql, array($idUser));
+            while (($enrBdd = $lectBdd->fetch()) != false)
+            { 
+                $this->array_groupes[] = new Sondage($enrBdd["id"]);
+            }   		
+	}
+
+	public function constructeurListePrive($idUser, $a, $b, $c , $d){
+		$sql='SELECT id_sondage
+				FROM user_sondage_votant
+				WHERE id_user=?
+				ORDER BY id DESC';
+				echo $idUser;
+		$lectBdd = $this->executerRequete($sql, array($idUser));
+            while (($enrBdd = $lectBdd->fetch()) != false)
+            { 
+                $this->array_groupes[] = new Sondage($enrBdd["id"]);
+            }   		
+	}
+	
+
+
+
 	public function getArraySondage(){
 		return $this->array_sondage;
 	}
@@ -70,11 +131,6 @@ class ListeSondage extends BD {
 		$this->array_sondage = $a;
 	}
 
-	/*public function afficherSond($idSond){
-
-		  	$this->array_sondage[] = new Sondage($idSond);
-
-	}*/
         
 
 	
